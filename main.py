610f4e4ef60d643c72cad495e26c85aef7fcae7c
Thor Jensen <thor@iwanoff.dk>
2023-03-22 14:21:11 +0100

    Merge from circles and walls

--- main.py
+++ main.py
@@ -42,14 +42,14 @@ while True:
     # Create a grayFrame
     grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     # Convert the frame to the HSV color space
-    blurFrame = cv2.GaussianBlur(frame, (11, 11), 0)
-    invFrame = cv2.bitwise_not(frame)
-    hsvFrame = cv2.cvtColor(invFrame, cv2.COLOR_BGR2HSV)
+    blurFrame = cv.GaussianBlur(frame, (11, 11), 0)
+    invFrame = cv.bitwise_not(frame)
+    hsvFrame = cv.cvtColor(invFrame, cv.COLOR_BGR2HSV)
     # Apply color mask to the frame to detect the red walls
-    wall_mask = cv2.inRange(hsvFrame, lower_wall_color, upper_wall_color)
+    wall_mask = cv.inRange(hsvFrame, lower_wall_color, upper_wall_color)
 
     # Find ping pong balls
-    circles = cv2.HoughCircles(grayFrame, cv2.HOUGH_GRADIENT, 1, 3,
+    circles = cv.HoughCircles(grayFrame, cv.HOUGH_GRADIENT, 1, 3,
                             param1=80, param2=17, minRadius=3, maxRadius=9)
     # param1 = sensitivity (smaller == more circles)
     # param2 = number of points in the circle (precision)
@@ -59,33 +59,33 @@ while True:
         circles = np.uint16(np.around(circles))
         for i in circles[0, :]:
             #Center of the circle
-            cv2.circle(frame, (i[0], i[1]), 1, (0, 0, 0), 2)
+            cv.circle(frame, (i[0], i[1]), 1, (0, 0, 0), 2)
 
             #Outer circle
-            cv2.circle(frame, (i[0], i[1]), i[2], (255,0,255), 2)
+            cv.circle(frame, (i[0], i[1]), i[2], (255,0,255), 2)
 
     # Find contours in the red wall mask
-    wall_contours, _ = cv2.findContours(wall_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    wall_contours, _ = cv.findContours(wall_mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
     # Loop over the red wall contours and draw as appropriate
     for wall_contour in wall_contours:
-        wall_area = cv2.contourArea(wall_contour)
+        wall_area = cv.contourArea(wall_contour)
         # For the outer wall, draw a red rectangle
         if 210000 > wall_area > 10000:
             # x, y, w, h = cv2.boundingRect(wall_contour)
             # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-            rect = cv2.minAreaRect(wall_contour)
-            box = cv2.boxPoints(rect)
+            rect = cv.minAreaRect(wall_contour)
+            box = cv.boxPoints(rect)
             box = np.intp(box)
-            cv2.drawContours(frame, [box], 0, (0, 255, 255), 2)
+            cv.drawContours(frame, [box], 0, (0, 255, 255), 2)
 
         # For the cross obstacle, mark the corners
         if 1300 > wall_area > 1000:
-            rect = cv2.minAreaRect(wall_contour)
-            box = cv2.boxPoints(rect)
+            rect = cv.minAreaRect(wall_contour)
+            box = cv.boxPoints(rect)
             box = np.intp(box)
             for coord in box:
-                cv2.circle(frame, (coord[0], coord[1]), 2, (0, 255, 255), 2)
+                cv.circle(frame, (coord[0], coord[1]), 2, (0, 255, 255), 2)
             # cv2.drawContours(frame, [box], 0, (0, 255, 255), 2)
 
             # Send red wall coordinates to server via HTTP
@@ -93,9 +93,10 @@ while True:
             # r = requests.post('http://example.com/wall_coordinates', data=wall_coordinates)
 
     # Display the resulting frame
-    cv2.imshow('frame', frame)
+    cv.imshow('frame', frame)
 
-    if cv2.waitKey(1) == ord('q'):
+    # If
+    if cv.waitKey(1) == ord('q'):
         break
 
 # Release the camera and close the window
