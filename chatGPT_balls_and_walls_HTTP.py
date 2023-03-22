154c1ef3a429cc98ae83c786ed5a4c8926e18954
Thor Jensen <thor@iwanoff.dk>
2023-03-22 14:03:24 +0100

    Added cross and angled rectangles

--- chatGPT_balls_and_walls_HTTP.py
+++ chatGPT_balls_and_walls_HTTP.py
@@ -8,6 +8,11 @@ import numpy as np
     If the area of the contour is greater than 100 pixels, it sends the objects
     coordinates to a server via HTTP POST request
 """
+# TODO Sample multiple images for the circles, only mark circles that appear in multiple
+# TODO Only map the border at the start, so the car can't obscure it
+# TODO Find a way to map the cross obstacle; what if we push it?
+# TODO Turn rectangles into coordinates
+
 
 # Set up camera capture
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
@@ -49,13 +54,26 @@ while True:
     # Find contours in the red wall mask
     wall_contours, _ = cv2.findContours(wall_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Loop over the red wall contours and draw a rectangle around each wall
+    # Loop over the red wall contours and draw as appropriate
     for wall_contour in wall_contours:
         wall_area = cv2.contourArea(wall_contour)
-        if 210000 > wall_area > 1000:
-            print(wall_area)
-            x, y, w, h = cv2.boundingRect(wall_contour)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        # For the outer wall, draw a red rectangle
+        if 210000 > wall_area > 10000:
+            # x, y, w, h = cv2.boundingRect(wall_contour)
+            # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            rect = cv2.minAreaRect(wall_contour)
+            box = cv2.boxPoints(rect)
+            box = np.intp(box)
+            cv2.drawContours(frame, [box], 0, (0, 255, 255), 2)
+
+        # For the cross obstacle, mark the corners
+        if 1300 > wall_area > 1000:
+            rect = cv2.minAreaRect(wall_contour)
+            box = cv2.boxPoints(rect)
+            box = np.intp(box)
+            for coord in box:
+                cv2.circle(frame, (coord[0], coord[1]), 2, (0, 255, 255), 2)
+            # cv2.drawContours(frame, [box], 0, (0, 255, 255), 2)
 
             # Send red wall coordinates to server via HTTP
             # wall_coordinates = {'x': x, 'y': y, 'w': w, 'h': h}
