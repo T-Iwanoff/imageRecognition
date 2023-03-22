50852c50208876c0f398d3f11c01fb45d244f2ed
Thor Jensen <thor@iwanoff.dk>
2023-03-22 09:17:13 +0100

    Added option to use video instead of camera

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 import cv2 as cv
 import numpy as np
 
-videoCapture = cv.VideoCapture(0, cv.CAP_DSHOW)
+videoPath = 'Video/Balls.mp4'
+video = True
+
+if video:
+    videoCapture = cv.VideoCapture(videoPath)
+else:
+    videoCapture = cv.VideoCapture(0, cv.CAP_DSHOW)
+
+if not videoCapture.isOpened():
+    print("Video file or camera not found")
+
+frameCounter = 0
 
 while True:
+    if video:
+        # If out of frames, reset the video
+        if frameCounter == videoCapture.get(7):  # propertyID 7 is the number of frames in the video
+            frameCounter = 0
+            videoCapture.set(1, 0)  # propertyID 1 is the current frame
+        frameCounter += 1
+
+    # Get the current frame
     ret, frame = videoCapture.read()
-    if not ret: break
+
+    if not ret:
+        break
 
     grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     invFrame = cv.bitwise_not(frame)
@@ -22,21 +43,23 @@ while True:
 
     # find circles
     circles = cv.HoughCircles(grayFrame, cv.HOUGH_GRADIENT, 1, 1,
-                              param1=70, param2=17, minRadius=1, maxRadius=8) #Param1 = sensitivity (smaller == more circles), param2 = number of points in the circle (precision)
+                              param1=70, param2=17, minRadius=1, maxRadius=8)  # Param1 = sensitivity (smaller == more circles), param2 = number of points in the circle (precision)
 
     if circles is not None:
         circles = np.uint16(np.around(circles))
         for i in circles[0, :]:
-            #Center of the circle
+            # Center of the circle
             cv.circle(showFrame, (i[0], i[1]), 1, (0, 0, 0), 2)
 
-            #Outer circle
+            # Outer circle
             cv.circle(showFrame, (i[0], i[1]), i[2], (255, 0, 255), 2)
 
     cv.imshow("Detection", mask)
     cv.imshow("Frame", showFrame)
 
-    if cv.waitKey(1) & 0xFF == ord('q'): break
+    # Press Q on keyboard to exit
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
 
 videoCapture.release()
 cv.destroyAllWindows()
