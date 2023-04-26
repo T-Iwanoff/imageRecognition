7d1e0e36674d20303e05e31f40ff0e3cbeaa8bd6
Thor Jensen <thor@iwanoff.dk>
2023-04-26 10:39:18 +0200

    Adjusted comments to circle parameters

--- constants.py
+++ constants.py
@@ -1,15 +1,14 @@
 
 # Circle detection constants
-# ?? param1 is sensitivity (smaller == more circles)
-# ?? param2 is number of points in the circle (precision)
-# ?? default ≈ param1=80, param2=17, minDist=3, minRadius=3, maxRadius=9
-# 1: 100-200 2: 20-30
+# param1 is sensitivity (smaller == more circles)
+# param2 is number of points in the circle (precision)
+# minDist is the minimum distance between circles. Meant to prevent overlapping circles
+# default ≈ param1=80, param2=17, minDist=3, minRadius=3, maxRadius=9
 CIRCLE_PARAM_1 = 200
 CIRCLE_PARAM_2 = 13
 CIRCLE_MIN_DIST = 9
 CIRCLE_MIN_RADIUS = 5
 CIRCLE_MAX_RADIUS = 7
-
 
 # Real world height and width of the course in meters
 COURSE_HEIGHT = 1.235
@@ -23,7 +22,6 @@ CUTOFF = 4
 # Define color ranges for red wall detection (inverted to cyan)
 LOWER_WALL_COLOR = (80, 70, 50)
 UPPER_WALL_COLOR = (100, 255, 255)
-
 
 # Wall area constants for wall detection
 # default ≈ min=10000, max=195000
