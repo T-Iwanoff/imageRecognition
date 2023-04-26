ac15a7bfe8fe9ac5d30a52c1e12eb80b64bfe251
Mark Bidstrup <markbidstrup@gmail.com>
2023-04-26 12:34:27 +0200

    removed print area

--- image_recognition/find_walls.py
+++ image_recognition/find_walls.py
@@ -7,7 +7,6 @@ def calibrate_wall_area(wall_area, find_obstacle):
     wall_area = sorted(wall_area, reverse=True)
     if find_obstacle:
         max_area = wall_area[1]
-        print("area: ", wall_area)
     else:
         max_area = wall_area[1]
     return max_area
