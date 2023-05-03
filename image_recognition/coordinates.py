459ddf432fe3a63e9a3c08876e46237926d99257
Thor Jensen <thor@iwanoff.dk>
2023-05-03 10:37:57 +0200

    Refactored course and coordinates

--- image_recognition/coordinates.py
+++ image_recognition/coordinates.py
@@ -2,28 +2,23 @@ import cv2 as cv
 import numpy as np
 from constants import *
 
-def coordinate_conversion(box, frame_x, frame_y):
-    # print(box)
-    if len(box) == 0:
+
+# Takes the walls of the course and the x and y coordinates of a point inside the course that you want to find
+# the meter-coordinates for (distance to the lower left corner on the x- and y-axis)
+def coordinate_conversion(walls, frame_x, frame_y):
+    if len(walls) == 0:
         return
-    if box[1][0]-box[0][0] == 0:  # If the box is detected upside down, return nothing
+    if walls[1][0] - walls[0][0] == 0:  # If the walls are detected upside down, return nothing
         return
 
-    inside_walls_x = frame_x - (box[0][0] + box[3][0])/2
-    x_scale = COURSE_WIDTH/(box[1][0]-box[0][0])
+    inside_walls_x = frame_x - (walls[0][0] + walls[3][0]) / 2
+    x_scale = COURSE_WIDTH / (walls[1][0] - walls[0][0])
     meter_x = inside_walls_x * x_scale
 
-    inside_walls_y = frame_y - (box[0][1] + box[1][1])/2
-    y_scale = COURSE_HEIGHT/(box[3][1]-box[0][1])
-    meter_y = (box[2][1]-frame_y) * y_scale
-    if (PRINT_COORDINATES):
+    inside_walls_y = frame_y - (walls[0][1] + walls[1][1]) / 2
+    y_scale = COURSE_HEIGHT / (walls[3][1] - walls[0][1])
+    meter_y = (walls[2][1] - frame_y) * y_scale
+    if PRINT_COORDINATES:
         print("x: ", meter_x)
         print("y: ", meter_y)
     return [meter_x, meter_y]
-
-
-# box = [[50, 400], [50, 30], [600, 30], [600, 400]]  # Straight box
-# box = [[47, 408], [55, 14], [592, 25], [584, 419]]  # Angled box
-# halfPoint_x = (box[3][0]-box[0][0])/2
-# halfPoint_y = (box[0][1]+box[1][1])/2
-# coordinate_convertion(box, halfPoint_x, halfPoint_y)
