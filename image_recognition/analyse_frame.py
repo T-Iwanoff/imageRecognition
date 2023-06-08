52425f0966de6e278539ace5ab5449ff2e7b04a0
Mark <s215782@student.dtu.dk>
2023-06-08 21:25:48 +0200

    orange circle show coords too

--- image_recognition/analyse_frame.py
+++ image_recognition/analyse_frame.py
@@ -155,14 +155,16 @@ def analyse_frame(frame, static_wall_corners=None, saved_circles=None, saved_ora
             for circle in circles:
                 improved_coords = improve_coordinate_precision(wall_corners, circle, "ball")
                 circles_in_meters.append(improved_coords)
-                # Used for drawing coords on frame
-                text = "x: " + str(round(improved_coords[0], 2)) + " y: " + str(round(improved_coords[1], 2))
+                text = "(" + str(round(improved_coords[0], 2)) + ", " + str(round(improved_coords[1], 2)) + ")"
                 # Draw coords on frame
-                cv.putText(frame, text, (circle[0]-50, circle[1]-20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                cv.putText(frame, text, (circle[0]-40, circle[1]-20), cv.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 1)
 
         if orange_circle is not None and len(orange_circle):
             improved_coords = improve_coordinate_precision(wall_corners, orange_circle, "ball")
             orange_circle_in_meters.append(improved_coords)
+            text = "(" + str(round(improved_coords[0], 2)) + ", " + str(round(improved_coords[1], 2)) + ")"
+            # Draw coords on frame
+            cv.putText(frame, text, (orange_circle[0] - 40, orange_circle[1] - 20), cv.FONT_HERSHEY_SIMPLEX, 0.4, (255, 0, 0), 1)
 
         if obstacle is not None:
             for coord in obstacle:
