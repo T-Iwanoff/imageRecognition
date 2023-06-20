fb52cc273dbfd7f21623eb8580226f536d4043fd
madspljoensson <madspljoensson@gmail.com>
2023-06-20 17:39:37 +0200

    hsv working

--- image_recognition/robotRecognition.py
+++ image_recognition/robotRecognition.py
@@ -44,17 +44,17 @@ def robot_recognition(frame, wall_corners, mask):
     # convert to hsv colorspace
     hsv = cv2.cvtColor(mask, cv2.COLOR_BGR2HSV)
 
-    # lower bound and upper bound for pointer color (light green)
-    lower_bound_pointer = np.array([50, 50, 20])
-    upper_bound_pointer = np.array([80, 100, 255])
+    # # lower bound and upper bound for pointer color (light green)
+    # lower_bound_pointer = np.array([50, 50, 20])
+    # upper_bound_pointer = np.array([80, 100, 255])
 
-    # lower bound and upper bound for center color (dark blue)
-    # lower_bound_center = np.array([110,60,50])
-    # upper_bound_center = np.array([140,255,255])
+    # # lower bound and upper bound for center color (dark blue)
+    # # lower_bound_center = np.array([110,60,50])
+    # # upper_bound_center = np.array([140,255,255])
 
-    # lower bound and upper bound for center color (blue)
-    lower_bound_center = np.array([90, 90, 50])
-    upper_bound_center = np.array([110, 250, 255])
+    # # lower bound and upper bound for center color (blue)
+    # lower_bound_center = np.array([90, 90, 50])
+    # upper_bound_center = np.array([110, 250, 255])
 
     # lower bound and upper bound for center color (lego blue)
     # lower_bound_center = np.array([110, 130, 20])
@@ -62,11 +62,11 @@ def robot_recognition(frame, wall_corners, mask):
 
     # HSV for the test day, based on robot.mp4.
     # lower bound and upper bound for pointer color (light green)
-    # lower_bound_pointer = np.array([40, 50, 20])
-    # upper_bound_pointer = np.array([80, 100, 255])
+    lower_bound_pointer = np.array([30, 30, 20])
+    upper_bound_pointer = np.array([80, 150, 255])
     # # lower bound and upper bound for center color (blue)
-    # lower_bound_center = np.array([80, 175, 50])
-    # upper_bound_center = np.array([110, 250, 255])
+    lower_bound_center = np.array([80, 105, 50])
+    upper_bound_center = np.array([110, 250, 255])
 
     # find the colors within the boundaries from center
     mask_center = cv2.inRange(hsv, lower_bound_center, upper_bound_center)
